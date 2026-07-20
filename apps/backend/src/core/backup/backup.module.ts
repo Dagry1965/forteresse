ee60bd62b9b011ad4b2ff14a6bfd9f38d3a14bea
Dagry Herve <ehivet@yahoo.fr>
2026-07-20 14:07:55 +0200

    fix: provide auth guards in backup module

--- apps/backend/src/core/backup/backup.module.ts
+++ apps/backend/src/core/backup/backup.module.ts
@@ -1,15 +1,21 @@
 ﻿import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { RolesGuard } from '../auth/roles.guard';
 import { BackupController } from './backup.controller';
 import { BackupService } from './backup.service';
 import { GoogleDriveService } from './google-drive.service';
 
 @Module({
+  imports: [
+    AuthModule,
+  ],
   controllers: [
     BackupController,
   ],
   providers: [
     BackupService,
     GoogleDriveService,
+    RolesGuard,
   ],
 })
 export class BackupModule {}
